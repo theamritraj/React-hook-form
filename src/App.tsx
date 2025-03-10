57f6767894a622dc847301fdede65db8db8e8d74
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2025-03-10 14:55:23 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+
+const App = () => {
+  return (
+    
+    <div className="container">
+      <form action="">
+        <input type="text" name="username" id=""/>
+        <input type="password" name="password"/>
+        <input type="submit"  value="submit" />
+      </form>
+    </div>
+  )
+}
+
+export default App
